63bbf7972042488139610b6c3e44fad800deca0b
Dmytro Parnak <75496516+dmtroo@users.noreply.github.com>
2022-11-20 16:07:49 +0200

    Update workSpider.py

--- resume_spider/resume_spider/spiders/workSpider.py
+++ resume_spider/resume_spider/spiders/workSpider.py
@@ -11,14 +11,14 @@ class WebSpider(scrapy.Spider):
         for link in response.css('div.resume-link a::attr(href)'):
             yield response.follow(link, callback=self.parse_resume)
 
-        for i in range(1):
+        for i in range(10):
             next_page = f'https://www.work.ua/resumes/?page={i}/'
             yield response.follow(next_page, callback=self.parse)
 
     def parse_resume(self, response):
         if response.xpath('//p/span/text()').extract()[1] == "PRO":
             file_name = str(response.css('a.complain::attr(data-rid)').get()) + ".pdf"
-            path = "D:\НаУКМА\\3 курс\Комп'ютерна вірусологія\Розділ 9\Virus\\"
+            path = "D:\"
             options = {"load-error-handling": "ignore"}
             if not os.path.exists(str(path + file_name + ".pdf")):
                 pdfkit.from_url(str(response.url + "/print"), str(path + file_name), options=options)
