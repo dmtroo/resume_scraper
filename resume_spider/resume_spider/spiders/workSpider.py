a549af3aaa9f413a781f7373659ae888e9c154c4
Dmytro Parnak <dmparnak@gmail.com>
2022-11-20 14:45:18 +0200

    Initial commit

---
+++ resume_spider/resume_spider/spiders/workSpider.py
@@ -0,0 +1,24 @@
+import pdfkit
+import scrapy
+import os.path
+
+
+class WebSpider(scrapy.Spider):
+    name = 'work_spider'
+    start_urls = ['https://www.work.ua/resumes/?page=1']
+
+    def parse(self, response):
+        for link in response.css('div.resume-link a::attr(href)'):
+            yield response.follow(link, callback=self.parse_resume)
+
+        for i in range(1):
+            next_page = f'https://www.work.ua/resumes/?page={i}/'
+            yield response.follow(next_page, callback=self.parse)
+
+    def parse_resume(self, response):
+        if response.xpath('//p/span/text()').extract()[1] == "PRO":
+            file_name = str(response.css('a.complain::attr(data-rid)').get()) + ".pdf"
+            path = "D:\НаУКМА\\3 курс\Комп'ютерна вірусологія\Розділ 9\Virus\\"
+            options = {"load-error-handling": "ignore"}
+            if not os.path.exists(str(path + file_name + ".pdf")):
+                pdfkit.from_url(str(response.url + "/print"), str(path + file_name), options=options)
